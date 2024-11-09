4647563e39e5789a6cff96e79d70aca01a2613c1
nicholas-gcc <nicholasgcc@gmail.com>
2024-11-10 00:16:19 +0800

    Set up unit tests for Dialogflow service

--- tests/testutils/constants.py
+++ tests/testutils/constants.py
@@ -2,6 +2,7 @@ import os
 
 DIALOGFLOW_PROJECT_ID = "querybot-auth0"
 DIALOGFLOW_LANGUAGE_CODE_EN = "en"
+DIALOGFLOW_FALLBACK_RESPONSE = "You must specify a valid argument to execute this query"
 AUTH0_BASE_URL = os.getenv("AUTH0_BASE_URL")
 AUTH0_CLIENT_ID = os.getenv("AUTH0_CLIENT_ID")
 AUTH0_CLIENT_SECRET = os.getenv("AUTH0_CLIENT_SECRET")
